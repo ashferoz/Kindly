32f0b6e46e6b70c28cb0fda39fff113ebf605d01
ashferoz <ash.safrina@gmail.com>
2024-07-19 18:35:54 +0800

    Set up backend file.

---
+++ server.ts
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+import express, { Express, Request, Response } from "express";
+const cors = require("cors");
+const helmet = require("helmet");
+const ratelimit = require("express-rate-limit");
+
+const limiter = ratelimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const app: Express = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(limiter);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello this is Ash. I am learning typescript!! Wish me luck");
+});
+
+app.get("/hi", (req: Request, res: Response) => {
+  res.send("Bye");
+});
+
+app.get("/yikes", (req: Request, res: Response) => {
+  res.send("oop");
+});
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
